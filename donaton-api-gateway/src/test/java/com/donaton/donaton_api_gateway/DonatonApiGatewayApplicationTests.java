ffbf6134eaf1b2f952aa134cc706696cd0a50cef
Servicios Digitales Concepcion <cetecom_conce@duoc.cl>
2026-05-12 16:41:08 -0400

    feat: configuracion de api gateway y conexion con eureka US-03

---
+++ donaton-api-gateway/src/test/java/com/donaton/donaton_api_gateway/DonatonApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donaton.donaton_api_gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonatonApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
