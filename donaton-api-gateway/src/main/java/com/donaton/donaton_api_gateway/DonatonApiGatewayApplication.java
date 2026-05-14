8802fa9b2556ccad6c8fc3e8a73400fed6611ee4
Servicios Digitales Concepcion <cetecom_conce@duoc.cl>
2026-05-14 16:59:43 -0400

    feat: api gateway registrado exitosamente en eureka US-03

--- donaton-api-gateway/src/main/java/com/donaton/donaton_api_gateway/DonatonApiGatewayApplication.java
+++ donaton-api-gateway/src/main/java/com/donaton/donaton_api_gateway/DonatonApiGatewayApplication.java
@@ -6,8 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DonatonApiGatewayApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DonatonApiGatewayApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(DonatonApiGatewayApplication.class, args);
+    }
 }
