e24db33173bce3a3261e4f5ba7f45143fac576ce
Servicios Digitales Concepcion <cetecom_conce@duoc.cl>
2026-05-14 18:26:42 -0400

    Avance en el authenticador

---
+++ donaton-auth/src/test/java/com/donaton/donaton_auth/DonatonAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donaton.donaton_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DonatonAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
