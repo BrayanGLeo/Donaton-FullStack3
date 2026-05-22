36dd2b52b8ec88e9fe1eff706391e80c86dc2aeb
Dani14789 <dan.godoys@duocuc.cl>
2026-05-22 01:16:16 -0400

    feat: implementar autenticacion stateless con jwt y endpoint login exitoso

---
+++ donaton-auth/src/main/java/com/donaton/donaton_auth/DonatonAuthApplication.java
@@ -0,0 +1,13 @@
+package com.donaton.donaton_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DonatonAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DonatonAuthApplication.class, args);
+	}
+
+}
