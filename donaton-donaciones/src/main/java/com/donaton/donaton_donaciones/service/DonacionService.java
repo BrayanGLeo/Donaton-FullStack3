40d622968ab62e511ac7accdab3a01db6eef7296
Your Name <br.godoys@duocuc.cl>
2026-05-24 22:46:25 -0400

    Solucion a problemas de test de donaciones

--- donaton-donaciones/src/main/java/com/donaton/donaton_donaciones/service/DonacionService.java
+++ donaton-donaciones/src/main/java/com/donaton/donaton_donaciones/service/DonacionService.java
@@ -3,7 +3,7 @@ package com.donaton.donaton_donaciones.service;
 import com.donaton.donaton_donaciones.config.RabbitMQConfig;
 import com.donaton.donaton_donaciones.entity.Donacion;
 import com.donaton.donaton_donaciones.repository.DonacionRepository;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.stereotype.Service;
 import java.time.LocalDateTime;
 import java.util.List;
@@ -12,9 +12,9 @@ import java.util.List;
 public class DonacionService {
 
     private final DonacionRepository repository;
-    private final RabbitTemplate rabbitTemplate;
+    private final AmqpTemplate rabbitTemplate;
 
-    public DonacionService(DonacionRepository repository, RabbitTemplate rabbitTemplate) {
+    public DonacionService(DonacionRepository repository, AmqpTemplate rabbitTemplate) {
         this.repository = repository;
         this.rabbitTemplate = rabbitTemplate;
     }
